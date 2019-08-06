de42aad5288241c6e10c57d371b6f2fd5f77ffb3
Marco Lewandowski <lewandowski@swms.de>
2019-08-06 16:55:12 +0200

    Projektdateien hinzufügen.

---
+++ IdentityServerCore/Config.cs
@@ -0,0 +1,34 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerCore
+{
+    public class Config
+
+    {
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "Customer Api Test")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
